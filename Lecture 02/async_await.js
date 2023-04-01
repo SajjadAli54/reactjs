f8aa6df4561b07ba3e0e9eb7ead46669c27c3bfc
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2023-04-01 10:29:46 +0500

    More on async and await

--- Lecture 02/async_await.js
+++ Lecture 02/async_await.js
@@ -34,3 +34,21 @@ console.log("Before calling promise");
     console.log("Error ", error);
   }
 })();
+
+async function callMe() {
+  return 1;
+}
+
+console.log(callMe()); // Promise { 1 }
+
+callMe().then((res) => console.log("Result in prmise then", res)); // Result 1
+
+(async function () {
+  console.log("Result in await/async ", await callMe()); // Result 1
+})();
+
+function callData() {
+  return getData();
+}
+
+console.log("Call Data", callData()); // Promise { <pending> }
