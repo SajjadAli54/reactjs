5309f2d16ae7b442fd833d07d6fa47a30f791df9
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2023-05-06 12:40:48 +0500

    Checkbox and radio buttons

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import SignupClass from "./Components/forms/class-signup";
 import Signup from "./Components/forms/signup";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <SignupClass />
     </div>
   );
 }
--- hello-world/src/Components/forms/class-signup.jsx
+++ hello-world/src/Components/forms/class-signup.jsx
@@ -6,31 +6,76 @@ export class SignupClass extends Component {
     this.state = {
       fullname: "",
       email: "",
+      gender: "",
+      about: "",
+      male: false,
+      TS: false,
+      react: false,
+      typescript: false,
+      age: 22,
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = (event) => {
+  handleChange(event) {
     // Event Synthesize object: event
-    this.setState(
-      {
-        [event.target.name]: event.target.value,
-      },
-      (prevState) => {
-        console.log(prevState.fullname);
-      }
-    );
-  };
+    let { name, value, type, checked } = event.target;
+    if (type === "checkbox") value = checked;
+    this.setState({ [name]: value });
+  }
 
   render() {
     return (
       <>
         <h1>Register</h1>
+        <p>{JSON.stringify(this.state)}</p>
         <form>
           <label>Name: </label>
-          <input type="text" name="fullname" onChange={this.handleChange} />
-          <br /> <br />
+          <input
+            type="text"
+            name="fullname"
+            value={this.state.fullname}
+            onChange={this.handleChange}
+          />
+
           <label>Email: </label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+
+          <label>About me</label>
+          <textarea
+            type="text"
+            name="about"
+            value={this.state.about}
+            onChange={this.handleChange}
+          />
+
+          <label>Skills</label>
+          <label>React:</label>
+          <input
+            type="checkbox"
+            name="react"
+            checked={this.state.react}
+            onChange={this.handleChange}
+          />
+          <label>Typescript:</label>
+          <input
+            type="checkbox"
+            name="typescript"
+            checked={this.state.typescript}
+            onChange={this.handleChange}
+          />
+
+          <label>Gender</label>
+          <input type="radio" name="male" value="Male" />
+          <label>Male</label>
+          <input type="radio" name="male" value="female" />
+          <label>Female</label>
         </form>
       </>
     );
